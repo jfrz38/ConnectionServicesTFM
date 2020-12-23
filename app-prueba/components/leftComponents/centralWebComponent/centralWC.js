b51dc292b51a474fa815ad9d87e83cd2f8cb287a
José Francisco Ruiz Zamora <jrz899@inlumine.ual.es>
2020-12-23 17:44:44 +0100

    Comunicación entre WebComponents en APP de prueba

---
+++ app-prueba/components/leftComponents/centralWebComponent/centralWC.js
@@ -0,0 +1,27 @@
+var label;
+
+class webComponent extends HTMLElement {
+
+    constructor() {
+        super()
+    }
+
+    async connectedCallback() {
+        console.log('my component is connected!');
+        await this.loadHTML()
+    }
+
+    async loadHTML() {
+        let res = await fetch('./components/leftComponents/centralWebComponent/centralWC.html')
+        this.attachShadow({ mode: 'open' })
+            .innerHTML = await res.text()
+        label = this.shadowRoot.getElementById("center-value")
+    }
+      update(details){
+        var operation = details.operation;
+        var value = parseInt(details.value);
+        label.innerText = operation === 'add' ? parseInt(label.innerText)+value : parseInt(label.innerText)-value
+      }
+}
+
+window.customElements.define('central-web-component', webComponent)
