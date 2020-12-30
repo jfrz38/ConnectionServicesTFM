19a86e2b60c125834171ba3ea5285e7221e1aa68
Ubuntu <jrz899@inlumine.ual.es>
2020-12-30 15:29:22 +0000

    Actualiza servidor wc2

--- iteration5/it1/wc2/src/server.js
+++ iteration5/it1/wc2/src/server.js
@@ -4,9 +4,7 @@ import render from './second-wc/render.js'
 const app = express();
 app.use('/wc2', express.static('./build'));
 
-app.use('/wc2-load', (req, res) => {
-    console.log("entra render: ",req.query)
-    console.log("render 2 = ",req.query.initialValue)
-    res.send(render(0));
+app.use('/wc2', (req, res) => {
+    res.send(render(req.query.initialValue));
 });
 app.listen(5103);
