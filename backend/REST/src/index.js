fe7b8b79d2ea3de09476cd152f2a29b548426b6b
José F. Ruiz <jrz899@inlumine.ual.es>
2021-07-13 23:45:29 +0000

    Actualiza puerto servidor REST

--- backend/REST/src/index.js
+++ backend/REST/src/index.js
@@ -4,7 +4,7 @@ const router = require('./routes/v1/data.route')
 const app = express()
 app.use('/', router)
 
-const port = 8080
+const port = 8081
 app.listen(port, () => {
   console.log(`Server listening on ${port}`)
 })
