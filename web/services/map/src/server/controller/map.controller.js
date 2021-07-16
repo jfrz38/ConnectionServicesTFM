e611d71f5d514c0175fbec521fb0dd0093a0e799
Ubuntu <jrz899@inlumine.ual.es>
2021-07-16 00:41:10 +0000

    Implementa servicio GraphQL

---
+++ web/services/map/src/server/controller/map.controller.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const global_model = mongoose.model('GlobalModel');
+const countries_summary_model = mongoose.model('CountriesSummaryModel');
+const metadata_model = mongoose.model('MetaDataModel');
+
+module.exports.getWorldMap = async () => {
+    const date = await getLastDateFromDB()
+    const country = await countries_summary_model
+        .find({ date: date })
+        .exec();
+    if (country.length == 0) {
+        return {}
+    }
+    var data = []
+    country.forEach(element => {
+        if (element.country == "France") element.country_iso2s[0] = "FR"
+        if (element.country == "United Kingdom") element.country_iso2s[0] = "GB"
+        if (element.country == "China") element.country_iso2s[0] = "CN"
+        data.push([element.country_iso2s[0], element.confirmed, element.deaths])
+    })
+    return {
+        data: data,
+        region: "world"
+    }
+
+}
+
+module.exports.getRegionMap = async (iso) => {
+    const date = await getLastDateFromDB()
+    const result = await countries_summary_model
+        .find({ date: date,
+            country_iso2s: iso.toUpperCase() })
+        .exec();
+    if (result.length == 0) {
+        return {}
+    }
+    var country = result[0]
+    if (country.country == "France") country.country_iso2s[0] = "FR"
+    if (country.country == "United Kingdom") country.country_iso2s[0] = "GB"
+    if (country.country == "China") country.country_iso2s[0] = "CN"
+    return {
+        data: [
+            ['Country', 'Confirmed', 'Deaths'],
+            [country.country_iso2s[0], country.confirmed, country.deaths]
+        ],
+        region: country.country
+    }
+}
+
+async function getLastDateFromDB() {
+    return new Promise(resolve => {
+        metadata_model
+            .find()
+            .exec((err, doc) => {
+                if (doc.length == 0) resolve(new Date());
+                else resolve(doc[0].last_date)
+            })
+    })
+}
--- web/services/map/src/server/index.js
+++ web/services/map/src/server/index.js
@@ -1,10 +1,35 @@
 var express = require('express');
-//const router = require('./routes/v1/index')
-var app = express()
-//require('./model/db')
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql')
+require('./model/db')
+const controller = require('./controller/map.controller')
 
+var app = express()
 app.use('/map', express.static('./build'));
 
-//app.use('/data', router)
+var schema = buildSchema(`
+type Query {
+  globalData: GlobalData
+  countryData (iso: String!): GlobalData
+}
+type GlobalData{
+  region: String
+  data: [[String]]
+}
+`);
 
+// The root provides a resolver function for each API endpoint
+var root = {
+    globalData: () => {
+        return controller.getWorldMap()
+    },
+    countryData: ({ iso }) => {
+        return controller.getRegionMap(iso)
+    }
+};
+app.use('/map', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 app.listen(6203);
